46f78f6fa2dcd5c98572e3e614194b9018e64b4e
MrMike262 <112904356+MrMike262@users.noreply.github.com>
2023-03-14 00:19:21 -0600

    Update serversocket.java

--- serversocket.java
+++ serversocket.java
@@ -1,57 +1,46 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.net.*;
+import java.io.*;
+import java.sql.*;
 
-public class ServidorSocket {
-    public static void main(String[] args) {
-        // Configuración de la conexión a la base de datos
-        String url = "jdbc:mysql://192.168.0.12:3306/sistemavotacion";
-        String usuario = "alumno";
-        String contraseña = "contrasena";
-        String puerto_del_servidor = "1234";
+public class SocketServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private Connection connection;
+    private Statement statement;
 
-        try {
-            // Cargar el controlador JDBC de MySQL
-            Class.forName("com.mysql.cj.jdbc.Driver");
+    public void start(int port) throws IOException {
+        // Establecer conexión con la base de datos
+        connection = DriverManager.getConnection("jdbc:mysql://192.168.0.12:3306/votos", "alumno", "contrasena");
+        statement = connection.createStatement();
 
-            // Crear un socket para escuchar conexiones entrantes
-            ServerSocket serverSocket = new ServerSocket(puerto_del_servidor);
+        // Establecer el socket y esperar a que un cliente se conecte
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
 
-            while (true) {
-                // Esperar a que un cliente se conecte
-                Socket socket = serverSocket.accept();
+        // Establecer los flujos de entrada y salida del socket
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                try {
-                    // Conectarse a la base de datos
-                    Connection conexion = DriverManager.getConnection(url, usuario, contraseña);
+        String inputLine;
 
-                    // Recibir la instrucción SQL del cliente
-                    DataInputStream dis = new DataInputStream(socket.getInputStream());
-                    String sql = dis.readUTF();
-
-                    // Ejecutar la instrucción SQL
-                    Statement statement = conexion.createStatement();
-                    int filasAfectadas = statement.executeUpdate(sql);
-
-                    // Enviar la respuesta al cliente
-                    String respuesta = filasAfectadas > 0 ? "Operación exitosa" : "Error al ejecutar la operación";
-                    DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-                    dos.writeUTF(respuesta);
-
-                    // Cerrar la conexión con la base de datos y el cliente
-                    conexion.close();
-                    socket.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        // Leer los datos enviados por el cliente y almacenarlos en la base de datos
+        while ((inputLine = in.readLine()) != null) {
+            statement.executeUpdate(inputLine);//"INSERT INTO dulces VALUES ('" + inputLine + "')"
+            out.println("OK");
         }
+
+        // Cerrar los flujos de entrada y salida y la conexión con la base de datos
+        out.close();
+        in.close();
+        connection.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        SocketServer server = new SocketServer();
+        server.start(1234);
     }
 }
